cf8b2caf91e75390b761d582c5ae682af30895bf
prystauka <skaryna@gmail.com>
2017-03-07 16:03:06 +0300

    Initial commit: create Rails app, install therubyracer

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  @hello = 'Ja umeju usio! Ура!'
+  end
+
+  def course
+    @lozung = 'jffffffffffds!'
+  end
+
+  def testimonials
+    @hey = 'KU KU'
+  end
+end
